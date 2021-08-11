9929801f171b5e626e99c1c8b05e53c464f46c47
Daniel Badura <daniel.badura@outlook.com>
2021-08-11 16:36:18 +0200

    Added basic routes

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 // jshint esversion: 9
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
-
-dotenv.config({path: "./config/config.env"});
+dotenv.config({ path: "./config/config.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,
-    console.log(`Server is running on ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.get("/movies", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all movies" });
+});
+app.post("/movies", (req, res) => {
+  res.status(200).json({ success: true, msg: "Create new movie" });
+});
+app.put("/movies/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Update movie ${req.params.id}` });
+});
+app.delete("/movies/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: `Remove movie  ${req.params.id}` });
+});
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running on ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
